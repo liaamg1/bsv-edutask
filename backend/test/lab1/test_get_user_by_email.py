4443ed87ee343498de10cab4261536c1f16219be
liaamg1 <lgustafsson71@gmail.com>
2026-04-06 13:06:46 +0200

    DocStrings

--- backend/test/lab1/test_get_user_by_email.py
+++ backend/test/lab1/test_get_user_by_email.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock
 from src.controllers.usercontroller import UserController
 
 def test_get_user_by_email_user_not_found ():
+    """None existing email returns nothing"""
     mock_dao = Mock()
     mock_dao.find.return_value = [] # No user found
     user_controller = UserController(mock_dao)
@@ -11,6 +12,7 @@ def test_get_user_by_email_user_not_found ():
     assert result is None
 
 def test_get_user_by_email_many_users_found ():
+    """Many of the same emails returns user, and warning"""
     mock_dao = Mock()
     mock_dao.find.return_value = [
         {"email": "local-part@domain.host"}, 
@@ -20,7 +22,9 @@ def test_get_user_by_email_many_users_found ():
     result = user_controller.get_user_by_email("local-part@domain.host")
 
     assert result["email"]=="local-part@domain.host"
+
 def test_get_user_by_email_valid_single_user_returns_user():
+    """Valid user email exists and returns user"""
     mock_dao=Mock()
     mock_dao.find.return_value = [{"email": "valid@email.com"}]
 
@@ -31,6 +35,7 @@ def test_get_user_by_email_valid_single_user_returns_user():
     assert result["email"]=="valid@email.com"
 
 def test_get_user_by_email_invalid_single_user_returns_error():
+    """Invalid email and returns error"""
     mock_dao=Mock()
 
     controller = UserController(mock_dao)
