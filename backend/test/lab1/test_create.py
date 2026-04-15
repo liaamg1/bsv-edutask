9a76817efff5a41a14a4cb41752b7d2a92629e43
mansbackstrom <mans.backstrom@hotmail.se>
2026-04-15 12:43:34 +0200

    changed env to get access to db, also created two integration tests

--- backend/test/lab1/test_create.py
+++ backend/test/lab1/test_create.py
@@ -1,5 +1,10 @@
 import pytest
-from unittest.mock import Mock
+from unittest.mock import patch
+from pymongo import MongoClient
+from dotenv import dotenv_values
+import os
+from pymongo.errors import WriteError
+
 from src.util.dao import DAO
 
 VALIDATOR = {
@@ -16,13 +21,18 @@ VALIDATOR = {
 
 @pytest.fixture
 def dao():
-    client = MongoClient("mongodb://localhost:27017/")
+    local_url = dotenv_values('.env').get('MONGO_URL')
+    mongo_url = os.environ.get('MONGO_URL', local_url) or "mongodb://localhost:27017/"
+    client = MongoClient(mongo_url)
     db = client["test_database"]
     collection = "users_test"
 
     db.drop_collection(collection)
 
     db.create_collection(collection, validator=VALIDATOR)
+
+    db[collection].create_index("email", unique=True)
+    db[collection].create_index("name", unique=True)
 
     with patch("src.util.dao.getValidator", return_value=VALIDATOR):
         dao = DAO(collection)
@@ -31,3 +41,21 @@ def dao():
 
     db.drop_collection(collection)
     client.close()
+
+@pytest.mark.unit
+def test_create_type_invalid(dao):
+    """Data containing the wrong data type raises WriteError"""
+    data = {"name": 123,"email":"local-part@domain.host"}
+
+    with pytest.raises(WriteError):
+        dao.create(data)
+
+@pytest.mark.unit
+def test_create_not_unique(dao):
+    """Data is not unique and raises WriteError"""
+    data = {"name": "123","email":"local-part@domain.host"}
+
+    dao.create(data)
+
+    with pytest.raises(WriteError):
+        dao.create(data)
