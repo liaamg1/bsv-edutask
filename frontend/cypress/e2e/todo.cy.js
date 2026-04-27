4f4c14eec6cbd2efdd7384980f5d99ce7892ecbb
mansbackstrom <mans.backstrom@hotmail.se>
2026-04-27 13:54:59 +0200

    feat: R8UC2 - Toggle todo-item as done or active - complete

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -29,7 +29,56 @@ function login(){
     
     
 }
+
 describe('R8UC1', () => {
+    login()
+
+    after(() => {
+        cy.request('DELETE', `http://localhost:5000/users/${uid}`)
+    })
+})
+
+describe('R8UC2 - Toggle todo-item as done or active', () => {
+    login()
+
+    it("Toggle todo-item as done", ()=> {
+        // Write Task Title
+        cy.get("#title").type("Go to the movies")
+        // Create Task
+        cy.get('input[value="Create new Task"]').click()
+        // Enter Task
+        cy.get(".title-overlay").contains("Go to the movies").click()
+        // Write Todo-Item
+        cy.get('input[placeholder="Add a new todo item"]').should('be.visible').type("Buy popcorn")
+        // Add Todo-Item
+        cy.get('input[value="Add"]').click()
+        // Click check box
+        cy.get(".checker.unchecked").first().click()
+    })
+
+    it("Toggle todo-item as active", () => {
+        // Write Task Title
+        cy.get("#title").type("Go to the films")
+        // Create Task
+        cy.get('input[value="Create new Task"]').click()
+        // Enter Task
+        cy.get(".title-overlay").contains("Go to the films").click()
+        // Write Todo-Item
+        cy.get('input[placeholder="Add a new todo item"]').should('be.visible').type("Buy popcorn")
+        // Add Todo-Item
+        cy.get('input[value="Add"]').click()
+        // Click check box
+        cy.get(".checker.unchecked").first().click()
+        // Click check box
+        cy.get(".checker.checked").first().click()
+    })
+
+    after(() => {
+        cy.request('DELETE', `http://localhost:5000/users/${uid}`)
+    })
+})
+
+describe('R8UC3 - Delete todo-item', () => {
     login()
 
     after(() => {
